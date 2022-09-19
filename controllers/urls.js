af8e6f3e6c2c5aee887d906f7379f0038b90ca01
DaweUrbi <urban.da@icloud.com>
2022-09-19 16:45:32 -0700

    generate random string for ID

--- controllers/urls.js
+++ controllers/urls.js
@@ -3,12 +3,22 @@
 const postNewUrl = async (req, res) => {
   const body = req.body;
   console.log("body", body);
-  const newUrl = { ...req.body};
+  const newUrl = { ...req.body, id: generateRandomString(6),};
   console.log("newUrl", newUrl);
   res.redirect("/urls");
 };
 
-
+const generateRandomString = (myLength) => {
+    const chars =
+      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+    const randomArray = Array.from(
+      { length: myLength },
+      (v, k) => chars[Math.floor(Math.random() * chars.length)]
+    );
+  
+    const randomString = randomArray.join("");
+    return randomString;
+  };
 
 
 module.exports = { postNewUrl };
