a8f7835c0793f5776a8483fee848710ca6379418
DaweUrbi <urban.da@icloud.com>
2022-09-19 16:44:41 -0700

    getting URL from input

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,1 +1,14 @@
 // urls functions
+
+const postNewUrl = async (req, res) => {
+  const body = req.body;
+  console.log("body", body);
+  const newUrl = { ...req.body};
+  console.log("newUrl", newUrl);
+  res.redirect("/urls");
+};
+
+
+
+
+module.exports = { postNewUrl };
