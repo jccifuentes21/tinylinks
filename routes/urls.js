0161bb497c7f736955dd589533b46afe4e9248e5
DaweUrbi <urban.da@icloud.com>
2022-09-20 16:17:48 -0700

    showUrls

--- routes/urls.js
+++ routes/urls.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { postNewUrl } = require("../controllers/urls");
+const { postNewUrl, showUrls } = require("../controllers/urls");
 
 router.post("/newUrl", postNewUrl);
-
+router.get("/urls", showUrls);
 
 module.exports = router;
