86b6c61b0acf1567bffad4bd85e6c31183787f55
DaweUrbi <urban.da@icloud.com>
2022-09-20 16:18:27 -0700

    export showUrls

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router;
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { postNewUrl } = require("./controllers/urls");
+const { postNewUrl, showUrls } = require("./controllers/urls");
+const fs = require("fs");
 
 const app = express();
 app.use(morgan("dev"));
@@ -11,28 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000);
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("urls", { title: 'Urls'});
-});
-
 app.get("/login", (req, res) => {
-  res.render("login", { title: 'Login'});
+	res.render("login", { title: "Login" });
 });
 
 app.get("/newUrl", (req, res) => {
-  res.render("newUrl", { title: 'New Url'});
+	res.render("newUrl", { title: "New Url" });
 });
 
 app.get("/register", (req, res) => {
-  res.render("register", { title: 'Register'});
+	res.render("register", { title: "Register" });
 });
 
 app.get("/singleUrl", (req, res) => {
-  res.render("singleUrl", { title: 'Url'});
-});
-
-app.get("/urls", (req, res) => {
-  res.render("urls", { title: 'Urls'});
+	res.render("singleUrl", { title: "Url" });
 });
 
 app.post("/newUrl", postNewUrl);
+app.get("/urls", showUrls);
+app.get("/", showUrls);
