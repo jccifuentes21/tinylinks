9b255494d13082c67b90d14572565226012dc8b8
DaweUrbi <urban.da@icloud.com>
2022-09-21 10:29:55 -0700

    delete URL from list

--- routes/urls.js
+++ routes/urls.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { postNewUrl, showUrls } = require("../controllers/urls");
+const { postNewUrl, showUrls, deleteUrl } = require("../controllers/urls");
 
 router.post("/newUrl", postNewUrl);
 router.get("/urls", showUrls);
+router.post("/delete/:id", deleteUrl);
 
 module.exports = router;
