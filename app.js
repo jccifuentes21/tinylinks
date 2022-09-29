b32e3eb0f4e390e4faf5469afc945451ce05870c
Juan Camilo <jccifuentes21@gmail.com>
2022-09-28 17:41:31 -0700

    added Procfile

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 app.use(cookieSession({
   name: 'session',
